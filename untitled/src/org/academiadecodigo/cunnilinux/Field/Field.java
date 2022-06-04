d72e37436d6ecbe70b07d5ce43e9c7f6f4872fef
dario7201 <codecadet@hopper.local>
2022-06-04 21:00:01 +0000

    first pic in Utils

---
+++ untitled/src/org/academiadecodigo/cunnilinux/Field/Field.java
@@ -0,0 +1,11 @@
+package org.academiadecodigo.cunnilinux.Field;
+
+
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class Field {
+   Picture picture = new Picture(0,0,"/Users/codecadet/Downloads/rsz_1chavi.png");
+   picture.draw();
+}
--- untitled/src/org/academiadecodigo/cunnilinux/Movement/Move.java
+++ untitled/src/org/academiadecodigo/cunnilinux/Movement/Move.java
@@ -2,25 +2,5 @@ package org.academiadecodigo.cunnilinux.Movement;
 
 public class Move {
     public void move() {
-        int direction = (int) Math.floor(Math.random() * 4);
-        if (!isDead()) {
-            switch (direction) {
-                case 0:
-                    moveUp();
-                    break;
-
-                case 1:
-                    moveDown();
-                    break;
-
-                case 2:
-                    moveRight();
-                    break;
-
-                case 3:
-                    moveLeft();
-                    break;
-            }
-        }
     }
 }
